be273af19d2a418a8d560853569b36215cbfce35
lrrichardso <lrrichardso@student.unimelb.edu.au>
2023-09-15 18:56:07 +1000

    Fuck u

--- Battleship.py
+++ Battleship.py
@@ -1,6 +1,6 @@
 import random
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit):
-    # This code is already flawless, no need to edit
+    # This code is beyond flawless
     x = random.randint(1,10)
     y = random.randint(1,10)
     return [x,y] 
