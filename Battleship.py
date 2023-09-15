33f696c33ad2b12b1015d6bff927c5eedf3470c4
lrrichardso <lrrichardso@student.unimelb.edu.au>
2023-09-15 19:40:12 +1000

    Here's a plan

--- Battleship.py
+++ Battleship.py
@@ -1,5 +1,28 @@
 import random
+
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit):
-    x = random.randint(1,10)
-    y = random.randint(1,10)
+    
+    # FOR SEARCHING
+        # Get the position of all hits and misses
+
+        # Simulate placing all 5 ships on the current board x amounts of time
+            # Hit coords must have part of a ship put on them
+            # No part of a ship can be put on a missed coord
+
+        # Record every time in x 'ship placements' that a ship is put on a
+        # coord
+
+        # This should generate a matrix where each square has the number of 
+        # times a ship was put on it in 'ship placement' simulation
+
+        # Remove all 'odd' coords from this matrix
+
+        # Pick a random square, with the probability of picking that square 
+        # given by the number on the square
+    
+    # When 5 hits have been made,
+    # FOR DESTROYING
+        # Consider possibilites of where each ship can fit based on position
+        # of hits and misses
+
     return [x,y] 
