ad6e5758ff71f3252c337e389fa42906914d804e
ianrac1 <ianrachinsky@gmail.com>
2023-09-16 16:58:31 +1000

    better visualisation tools

--- Battleship.py
+++ Battleship.py
@@ -6,9 +6,9 @@ def print_shots(storage):
 			if storage[i][j] == -1:
 				matrix_zeros[i][j] = 'X'
 			elif storage[i][j] == 1:
-				matrix_zeros[i][j] = '.'
+				matrix_zeros[i][j] = '0'
 			else:
-				matrix_zeros[i][j] = ' '
+				matrix_zeros[i][j] = '.'
 	print_grid(matrix_zeros)
 		
 		
@@ -31,6 +31,8 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
     #print_grid(heatmap)
     # Choose a coord from the heatmap
     heatmap = heatmap_zeros(storage, heatmap)
+
+    print_grid(heatmap)
 
     coord = select_from_heatmap(heatmap)
 
@@ -104,16 +106,16 @@ def heatmap_hits(storage, heatmap):
             if storage[r][c] == HIT:
                 # check left
                 if valid_coord(r, c-1, heatmap):
-                    heatmap[r][c-1] += 100
+                    heatmap[r][c-1] += 5
                 # check right
                 if valid_coord(r, c+1, heatmap):
-                    heatmap[r][c+1] += 100
+                    heatmap[r][c+1] += 5
                 # check above
                 if valid_coord(r-1, c, heatmap):
-                    heatmap[r-1][c] += 100
+                    heatmap[r-1][c] += 5
                 # check below
                 if valid_coord(r+1, c, heatmap):
-                    heatmap[r+1][c] += 100
+                    heatmap[r+1][c] += 5
     return heatmap
 
 def heatmap_zeros(storage, heatmap):
@@ -252,6 +254,8 @@ def get_heat_map(storage, heatmap):
 	return calculate_heat_map(storage, heatmap)
 
 def print_grid(array):
-    for r in array:
-        print(r)
-    print("")
+    for r in range(10):
+        for c in range(10):
+            print(array[r][c], end="")
+        print()
+    print()
