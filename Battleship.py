46c8399b6130be8c12650522c9c46e2a2501ae5c
ianrac1 <ianrachinsky@gmail.com>
2023-09-16 11:14:26 +1000

    Add New Files

--- Battleship.py
+++ Battleship.py
@@ -1,6 +1,5 @@
 import random
-def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit):
-    # This code is not flawless, it must be edited
+def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
     x = random.randint(1,10)
     y = random.randint(1,10)
-    return [x,y] 
+    return [x,y], storage
