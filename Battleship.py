2b80865689b52c0da1aef65df75cba7def019dec
lrrichardso <lrrichardso@student.unimelb.edu.au>
2023-09-16 14:17:15 +1000

    Testing phase

--- Battleship.py
+++ Battleship.py
@@ -5,8 +5,8 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
     storage = update_storage(storage, p1ShotSeq, p1PrevHit)
 
     # Creating a heatmap
-    heatmap = []
-    heatmap = zero_heatmap(heatmap)
+    heatmap = zero_array()
+    print(heatmap)
     heatmap = get_heatmap(storage, heatmap)
     heatmap = heatmap_hits(storage, heatmap)
     
@@ -15,6 +15,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
 
     # Return this coordinate as a guess
     x, y = coord[0], coord[1]
+    # print(f"x: {x}, y: {y}")
     return [x,y], storage
     
 
@@ -59,21 +60,18 @@ def update_storage(storage, p1ShotSeq, p1PrevHit):
         return storage
     else:
         #initialise
-        initStorage = []
-        for row in range(10):
-            initInnerRow = 10*[UNKNOWN]
-            initStorage.append(initInnerRow)
-        storage = initStorage
+        storage = zero_array()
         return storage
 
 
-def zero_heatmap(heatmap):
+def zero_array():
     """ Set the heatmap to all zeros """
-    for x in range(10):
-        heatmap.append([])
-        for y in range(10):
-            heatmap[x][y] = 0
-    return heatmap
+    initArray = []
+    UNKNOWN = 0
+    for row in range(10):
+        initInnerRow = 10*[UNKNOWN]
+        initArray.append(initInnerRow)
+    return initArray
 
 
 def heatmap_hits(storage, heatmap):
@@ -115,14 +113,14 @@ def select_from_heatmap(heatmap):
         for y in range(10):
             heat_val = heatmap[x][y]
             if heat_val == max_heat_val:
-                max_coords.append((x, y))
+                max_coords.append([x, y])
     
     # Break tiebreakers between items in max_coords
     if len(max_coords) > 1:
         # Only take coordinates along diagonals to choose from a checkerboard
         # pattern of coordinates for more efficient guessing
         for coord in max_coords:
-            if ((coord[0] + coord[1])/2 != 0):
+            if ((coord[0] + coord[1])%2 != 0 and len(max_coords) > 1):
                 max_coords.remove(coord)
         
     # If there are still multiple coords left after this process, choose 
