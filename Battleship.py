96a8a5504baca6edefe304b1142f18d9485aa0bd
lrrichardso <lrrichardso@student.unimelb.edu.au>
2023-09-16 11:06:53 +1000

    Added random coords back into the game

--- Battleship.py
+++ Battleship.py
@@ -1,7 +1,9 @@
 import random
 
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit):
-    
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+
     # FOR SEARCHING
         # Get the position of all hits and misses
 
