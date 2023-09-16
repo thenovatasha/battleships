cffe867bf2cc99095c4be3507ce168e5072a9b37
lrrichardso <lrrichardso@student.unimelb.edu.au>
2023-09-16 11:25:41 +1000

    Added a debug info printing function

--- Battleship.py
+++ Battleship.py
@@ -1,5 +1,16 @@
 import random
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
+    print_key_info(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage)
     x = random.randint(1,10)
     y = random.randint(1,10)
     return [x,y], storage
+
+
+def print_key_info(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
+    print(f"Round: {round}")
+    print(f"Yourmap: {yourMap}")
+    print(f"yourHp: {yourHp}")
+    print(f"enemyHp: {enemyHp}")
+    print(f"p1ShotSeq: {p1ShotSeq}")
+    print(f"p1PrevHit: {p1PrevHit}")
+    print(f"Storage: {storage}")
